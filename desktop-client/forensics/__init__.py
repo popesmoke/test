ce90de2cf89f18a23935f9c29e7adf0902dc697c
popesmoke <popesmoke@users.noreply.github.com>
2026-05-14 17:52:05 +0300

    Revert forensic modules commit

--- desktop-client/forensics/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Windows forensic correlation modules for anti-cheat diagnostics."""
-
-from .orchestrator import run_windows_forensic_correlation
-
-__all__ = ["run_windows_forensic_correlation"]
